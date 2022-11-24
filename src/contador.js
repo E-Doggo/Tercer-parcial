540335a53a33665dab451824bbf46b98303c0aa0
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-11-24 09:35:00 -0400

    Devuelve de varias palabras con las repeticiones

--- src/contador.js
+++ src/contador.js
@@ -1,8 +1,15 @@
 function contar(texto) {
   let arrText = texto.split(' ')
-  let resultado ='';
+  let resultado ={};
   for(let i = 0;i <arrText.length; i++){
-     resultado += `${arrText[i]} = 1 `
+    var palabra = arrText[i];
+    if(resultado[palabra] == undefined){
+      resultado[palabra] = 1;
+    }
+    else{
+      resultado[palabra] += 1;
+    }
+    //  resultado += `${arrText[i]} = 1 `
 
   }
   return resultado;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,9 +2,14 @@ import contar from "./contador.js";
 
 describe("Deberia darnos una cuenta de las palabras tomando en cuenta capitalizacion", () => {
   it("Nos devolvera una sola palabra", () => {
-    expect(contar("palabra")).toEqual("palabra = 1 ");
+    expect(contar("palabra")).toEqual({"palabra": 1});
   });
   it("Nos devolvera de varias palabras", () => {
-    expect(contar("palabra tiene letras")).toEqual("palabra = 1 tiene = 1 letras = 1 ");
+    expect(contar("palabra tiene letras")).toEqual({"letras": 1, "palabra": 1, "tiene": 1});
   });
+  it("Nos devolvera de varias palabras", () => {
+    expect(contar("palabra tiene letras y palabra es oracion")).toEqual({"es": 1, "letras": 1, "oracion": 1, "palabra": 2, "tiene": 1, "y": 1});
+  });
+
 });
+
